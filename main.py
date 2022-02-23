a3db4f2625a804d763c05cdfca4f3b5b7cb8e1df
Design8 <u41@Denaco.com>
2022-02-23 09:35:37 +0330

    create subfolders in main folders

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import os
 with open('product-names.txt') as pr:
     product_list = [line.rstrip('\n') for line in pr]
 
+with open('subfolder-names.txt') as sub:
+    subfolder_list = [line.rstrip('\n') for line in sub]
+
 for products in product_list:
     os.mkdir(products)
     
+for subfolders in subfolder_list:
+        os.mkdir(products + '/' + subfolders)
